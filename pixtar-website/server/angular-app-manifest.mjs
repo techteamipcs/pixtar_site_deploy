9fca764d40ca6104352c0d399f1c734431749f3e
Muzaffar <muzaffar.ipcs@gmail.com>
2026-05-26 17:46:02 +0530

    new dep

--- pixtar-website/server/angular-app-manifest.mjs
+++ pixtar-website/server/angular-app-manifest.mjs
@@ -8,7 +8,7 @@ export default {
   {
     "renderMode": 0,
     "preload": [
-      "chunk-SGZDJWJI.js",
+      "chunk-3UPH4OHN.js",
       "chunk-SRX5CWNT.js",
       "chunk-6RYCBRBG.js"
     ],
@@ -26,7 +26,7 @@ export default {
   {
     "renderMode": 0,
     "preload": [
-      "chunk-EGL7DA3K.js",
+      "chunk-K2RWW3JM.js",
       "chunk-VEHEYVDZ.js",
       "chunk-ZUJEXEEH.js",
       "chunk-YYVCPK3E.js",
@@ -39,8 +39,8 @@ export default {
   {
     "renderMode": 0,
     "preload": [
-      "chunk-WXASNP5J.js",
-      "chunk-IQ2SFSFB.js",
+      "chunk-DGH3UGOM.js",
+      "chunk-OITOSMKA.js",
       "chunk-HIG45VMH.js",
       "chunk-HXMFB5QU.js",
       "chunk-XEB35I3N.js",
@@ -55,8 +55,8 @@ export default {
   {
     "renderMode": 0,
     "preload": [
-      "chunk-PEITSTI7.js",
-      "chunk-IQ2SFSFB.js",
+      "chunk-3ABRL22B.js",
+      "chunk-OITOSMKA.js",
       "chunk-HIG45VMH.js",
       "chunk-HXMFB5QU.js",
       "chunk-XEB35I3N.js",
@@ -71,8 +71,8 @@ export default {
   {
     "renderMode": 0,
     "preload": [
-      "chunk-3TEQ24MH.js",
-      "chunk-IQ2SFSFB.js",
+      "chunk-NGTS23F2.js",
+      "chunk-OITOSMKA.js",
       "chunk-HIG45VMH.js",
       "chunk-HXMFB5QU.js",
       "chunk-XEB35I3N.js",
@@ -87,8 +87,8 @@ export default {
   {
     "renderMode": 0,
     "preload": [
-      "chunk-FZ6KVPEA.js",
-      "chunk-IQ2SFSFB.js",
+      "chunk-W42NN2FF.js",
+      "chunk-OITOSMKA.js",
       "chunk-HIG45VMH.js",
       "chunk-HXMFB5QU.js",
       "chunk-XEB35I3N.js",
@@ -124,7 +124,7 @@ export default {
   {
     "renderMode": 0,
     "preload": [
-      "chunk-VZUHP237.js",
+      "chunk-YDADLBLF.js",
       "chunk-HIG45VMH.js"
     ],
     "route": "/projects"
@@ -132,7 +132,7 @@ export default {
   {
     "renderMode": 0,
     "preload": [
-      "chunk-3BJI7MZO.js",
+      "chunk-OKFEOOBA.js",
       "chunk-YYVCPK3E.js",
       "chunk-6RYCBRBG.js"
     ],
@@ -259,8 +259,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 16098, hash: 'a6e95a17889ab5c1af5a84fe6146cb0cc34e28f982324cc31dbd7d44e719d014', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 13922, hash: '51d1a441d98d4a6f8ff5e085bfb77c75ea563100002c3fd017ec4b22e2f74183', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-XLAEXQYJ.css': {size: 13604, hash: 'y8qtb7k2lC0', text: () => import('./assets-chunks/styles-XLAEXQYJ_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 16098, hash: 'd40ba2f0923b925ca37628ad1dc89efb96d0c655c2045058600493454dc595bd', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 13922, hash: 'e055ae1074dfe6801d97adb31a0a85822f69b5937e755b25f00bbc220d619f6e', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-BVTSGQGK.css': {size: 13603, hash: '/6u687dp3uM', text: () => import('./assets-chunks/styles-BVTSGQGK_css.mjs').then(m => m.default)}
   },
 };
